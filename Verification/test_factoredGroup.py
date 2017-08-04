425a5950543c2c4b4ef98049c19768e4b98dd211
skane88 <sean.kane@outlook.com.au>
2017-08-04 10:21:55 +1000

    Finished unit tests for the FactoredGroup class.

--- Verification/test_factoredGroup.py
+++ Verification/test_factoredGroup.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from LoadGroup import FactoredGroup
+from LoadGroup import FactoredGroup, LoadFactor
 from Load import Load, RotatableLoad, ScalableLoad, WindLoad
 
 class TestFactoredGroup(TestCase):
@@ -70,4 +70,60 @@ class TestFactoredGroup(TestCase):
         self.assertEqual(first = LG.load_factors, second = load_factors)
 
     def test_generate_cases(self):
-        self.fail()
+        """
+        Test the generate_cases function that generates the return load
+        combinations
+        """
+
+        l1 = Load(load = 'G1 - Mechanical Dead Load', load_no = 1,
+                  abbrev = 'G1')
+        l2 = ScalableLoad(load = 'Q1 - 5kPa Live Load', load_no = 2,
+                          load_value = 5, abbrev = 'Q1')
+        l3 = RotatableLoad(load = 'R1 - Rotating Load', load_no = 3,
+                           load_value = 10, angle = 45.0, symmetrical = True,
+                           abbrev = 'R1')
+        l4 = WindLoad(load = 'WUx - Wind Load', load_no = 4, wind_speed = 69.0,
+                      angle = 0.0, symmetrical = True, abbrev = 'WUx')
+
+        group_name = 'Group 1'
+        loads = [l1, l2, l3, l4]
+        load_factors = (-1.0, 0.0, 1.0)
+        abbrev = 'GP 1'
+
+        LG = FactoredGroup(group_name = group_name, loads = loads,
+                           load_factors = load_factors, abbrev = abbrev)
+
+        LC1_1 = LoadFactor(load = l1, load_factor = -1.0, add_info = '')
+        LC2_1 = LoadFactor(load = l2, load_factor = -1.0, add_info = '')
+        LC3_1 = LoadFactor(load = l3, load_factor = -1.0, add_info = '')
+        LC4_1 = LoadFactor(load = l4, load_factor = -1.0, add_info = '')
+
+        LC_1 = (LC1_1, LC2_1, LC3_1, LC4_1)
+
+        LC1_2 = LoadFactor(load = l1, load_factor = 0.0, add_info = '')
+        LC2_2 = LoadFactor(load = l2, load_factor = 0.0, add_info = '')
+        LC3_2 = LoadFactor(load = l3, load_factor = 0.0, add_info = '')
+        LC4_2 = LoadFactor(load = l4, load_factor = 0.0, add_info = '')
+
+        LC_2 = (LC1_2, LC2_2, LC3_2, LC4_2)
+
+        LC1_3 = LoadFactor(load = l1, load_factor = 1.0, add_info = '')
+        LC2_3 = LoadFactor(load = l2, load_factor = 1.0, add_info = '')
+        LC3_3 = LoadFactor(load = l3, load_factor = 1.0, add_info = '')
+        LC4_3 = LoadFactor(load = l4, load_factor = 1.0, add_info = '')
+
+        LC_3 = (LC1_3, LC2_3, LC3_3, LC4_3)
+
+        LC = (LC_1, LC_2, LC_3)
+
+        print(LC[0])
+        print(LC[1])
+        print(LC[2])
+
+        LC_act = tuple(LG.generate_cases())
+
+        print(LC_act[0])
+        print(LC_act[1])
+        print(LC_act[2])
+
+        self.assertEqual(first = tuple(LG.generate_cases()), second = LC)
