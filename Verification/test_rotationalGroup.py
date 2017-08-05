ead4f370c37a86dec9100920560d2c93a1abb1d5
skane88 <sean.kane@outlook.com.au>
2017-08-05 20:38:34 +1000

    Added test case for the RotationalGroup

---
+++ Verification/test_rotationalGroup.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+"""
+Tests for the RotataionalGroup class.
+"""
+
+from unittest import TestCase
+
+
+class TestRotationalGroup(TestCase):
+
+    def test_basic(self):
+        self.fail()
+
+    def test_loads(self):
+        self.fail()
+
+    def test_half_list(self):
+        self.fail()
+
+    def test_interp_func(self):
+        self.fail()
+
+    def test_req_angles(self):
+        self.fail()
+
+    def test_set_req_angles_int(self):
+        self.fail()
+
+    def test_generate_cases(self):
+        self.fail()
