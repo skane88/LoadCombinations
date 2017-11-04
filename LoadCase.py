ab741659ba8598c72a9caecf6b1f716dfc20b867
skane88 <sean.kane@outlook.com.au>
2017-11-04 10:18:50 +1000

    Continued work on LoadCase class.

--- LoadCase.py
+++ LoadCase.py
@@ -16,6 +16,16 @@ class LoadCase:
     to generate appropriate load combinations.
     """
 
+    def __init__(self, case_name: str, case_no: int,
+                 load_groups: Union[Dict[str, LoadGroup],
+                                    List[LoadGroup], LoadGroup],
+                 abbrev: str = ''):
+
+        self.case_name = case_name
+        self.case_no = case_no
+        self.load_groups = load_groups
+        self.abbrev = abbrev
+
     @property
     def case_name(self) -> str:
         return self._case_name
@@ -33,16 +43,22 @@ class LoadCase:
         self._case_no = case_no
 
     @property
-    def load_groups(self) -> :
+    def load_groups(self) -> Dict[str, LoadGroup]:
         return self._load_groups
 
     @load_groups.setter
     def load_groups(self,
-                    load_group: Union[Dict[str, LoadGroup],
+                    load_groups: Union[Dict[str, LoadGroup],
                                       List[LoadGroup], LoadGroup]):
+
+        # if setting via load_groups, the assumption is that the entire
+        # self._load_groups object is being overwritten:
+
         self._load_groups = {}
 
-        self.add_group(load_group)
+        # use the add_group method to add LoadGroups, to check for duplicates
+        # etc.
+        self.add_group(load_groups)
 
     def add_group(self,
                   load_group: Union[Dict[str, LoadGroup],
@@ -50,7 +66,7 @@ class LoadCase:
 
         if isinstance(load_group, Dict):
 
-            for k, lg in load_group:
+            for k, lg in load_group.items():
                 self.add_group(lg)
 
         elif isinstance(load_group, List):
@@ -60,7 +76,7 @@ class LoadCase:
 
         else:
 
-            if self.group_exists(load_group) == False:
+            if self.group_exists(load_group = load_group) == False:
                 self._load_groups[load_group.group_name] = load_group
             else:
                 raise LoadGroupExistsException(f'Attempted to add a LoadGroup'
@@ -70,7 +86,7 @@ class LoadCase:
                                                + f', LoadCase.load_groups:'
                                                + f' {str(self.load_groups)}.')
 
-    def del_group(self, group_name: str = None,
+    def del_group(self, *, group_name: str = None,
                        load_group: LoadGroup = None):
 
         group_present = self.group_exists(group_name = group_name,
@@ -82,10 +98,45 @@ class LoadCase:
             raise LoadGroupNotPresentException(f'Attempted to delete LoadGroup'
                                                + f' which did not exist.')
 
-    def group_exists(self, group_name: str = None,
+    def group_exists(self, *, group_name: str = None,
                      load_group: LoadGroup = None):
 
-        raise NotImplementedError
+        # if the self._load_groups property is empty the load_group obviously
+        # cannot exist.
+
+        if len(self.load_groups) == 0:
+            return False
+
+        # else need to search for the LoadGroup
+
+        if group_name != None:
+
+            if group_name in self.load_groups:
+                return group_name
+            else:
+                return False
+
+        elif load_group != None:
+
+            for k, lg in self.load_groups.items():
+
+                if load_group.group_name == k or lg == load_group:
+                    return k
+
+            # else haven't found it so return False
+            return False
+
+        else:
+            raise ValueError(f'To check if a LoadGroup exists a LoadGroup needs'
+                             + f' to be provided. No information provided.')
+
+    @property
+    def abbrev(self) -> str:
+        return self._abbrev
+
+    @abbrev.setter
+    def abbrev(self, abbrev: str):
+        self._abbrev = abbrev
 
     def __str__(self):
         raise NotImplementedError
