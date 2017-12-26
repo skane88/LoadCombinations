9729c4b006225dab6675d1d3660c4eddc573c4c0
skane88 <sean.kane@outlook.com.au>
2017-12-27 09:22:03 +1000

    Added GroupFactor class (empty at the moment).

---
+++ Factors.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+This module contains a pair of helper classes for ``LoadCase`` factors and
+``LoadGroup`` factors.
+"""
+
+class GroupFactor:
+    """
+    This class is a helper class that combines a ``LoadGroup`` with a factor to
+    multiply the resulting combinations with.
+    """
