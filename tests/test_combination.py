001c2f9f74a81c992e6694d8c9926d4fbda97e19
skane88 <sean.kane@outlook.com.au>
2018-01-04 11:30:11 +1000

    Added test class for Combination class.

---
+++ tests/test_combination.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+
+class TestCombination(TestCase):
+    def test_load_factors(self):
+        self.fail()
+
+    def test_add_load_factor(self):
+        self.fail()
+
+    def test_del_load(self):
+        self.fail()
+
+    def test_load_exists(self):
+        self.fail()
+
+    def test_load_factor_exists(self):
+        self.fail()
+
+    def test_allow_duplicates(self):
+        self.fail()
+
+    def test_load_case(self):
+        self.fail()
+
+    def test_load_case_abbrev(self):
+        self.fail()
+
+    def test_load_case_no(self):
+        self.fail()
+
+    def test_combination_title(self):
+        self.fail()
+
+    def test_list_load_factors(self):
+        self.fail()
+
+    def test_list_loads(self):
+        self.fail()
+
+    def test_list_loads_with_factors(self):
+        self.fail()
