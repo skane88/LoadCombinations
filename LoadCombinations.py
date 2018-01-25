d23b661c75250734ef16c99c960d41b0e1d48d9d
skane88 <sean.kane@outlook.com.au>
2018-01-25 20:58:01 +1000

    Added file for LoadCombinations class.

---
+++ LoadCombinations.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+"""
+This contains a class describing a ``LoadCombinations`` object. This will be
+used to combine Loads, LoadGroups and LoadCases together into a single simple
+object for use by the end user.
+"""
