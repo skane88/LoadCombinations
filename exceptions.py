eb92041ef08e74bd3e3f68bcf847bb1b0957b8cc
skane88 <sean.kane@outlook.com.au>
2018-02-15 21:45:09 +1000

    Updated exception file.

--- exceptions.py
+++ exceptions.py
@@ -4,14 +4,23 @@
 This file defines custom exceptions as required.
 """
 
-class LoadExistsException(Exception):
+class LoadCombinationException(Exception):
+    """
+    Creates a base excepction for the LoadCombination project, so that all
+    custom exceptions can be inherited, allowing other users to catch all
+    exceptions from this library in one place.
+    """
+
+    pass
+
+class LoadExistsException(LoadCombinationException):
     """
     This exception is raised when a load already exists in a LoadGroup object.
     """
 
     pass
 
-class LoadNotPresentException(Exception):
+class LoadNotPresentException(LoadCombinationException):
     """
     This exception is raised when a load does not exist in a LoadGroup object
     but is expected by a method.
@@ -27,7 +36,7 @@ class AngleExistsException(LoadExistsException):
 
     pass
 
-class LoadGroupExistsException(Exception):
+class LoadGroupExistsException(LoadCombinationException):
     """
     This exception is raised when a ``LoadGroup`` already exists in a
     ``LoadCase`` object.
@@ -35,7 +44,7 @@ class LoadGroupExistsException(Exception):
 
     pass
 
-class LoadGroupNotPresentException(Exception):
+class LoadGroupNotPresentException(LoadCombinationException):
     """
     This exception is raised when a ``LoadGroup`` does not exist in a
     ``LoadCase`` object but is expected by a method.
@@ -43,7 +52,7 @@ class LoadGroupNotPresentException(Exception):
 
     pass
 
-class InvalidCombinationFactor(Exception):
+class InvalidCombinationFactor(LoadCombinationException):
     """
     This exception is raised when the value passed into a ``LoadCase`` for the
     ``(LoadGroup, combination_factor)`` is not a ``Tuple[LoadGroup, float]``.
